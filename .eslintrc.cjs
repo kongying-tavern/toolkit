dcebb8f5486039e34e3a19eb121e1a1e75e35e54
Alex Fang <deeno@qq.com>
2023-10-10 06:32:05 +0800

    :fire: remove template default codes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true }
     ],
+    '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': 'warn'
   },
 }
--- electron/preload.ts
+++ electron/preload.ts
@@ -109,6 +109,7 @@ function useLoading() {
 
 // ----------------------------------------------------------------------
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const { appendLoading, removeLoading } = useLoading();
 domReady().then(appendLoading);
 
