802f70bc1a4a58fbca3405201a131265069696ba
夕云葛城 <KuroNeko7520@outlook.com>
2024-07-30 23:07:33 +0800

    :bug: (electron-env.d.ts) type fix

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -23,5 +23,9 @@ declare namespace NodeJS {
 
 // Used in Renderer process, expose in `preload.ts`
 interface Window {
-  ipcRenderer: import("electron").IpcRenderer;
+  versions: {
+    node: string;
+    chrome: string;
+    electron: string;
+  };
 }
