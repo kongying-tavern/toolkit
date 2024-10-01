5e7faf6bbedb071ed9b6cce21518b320b0f5b7b8
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-02 02:40:33 +0800

    :bento: (resource) 添加 SegoeIcons 字体图标

---
+++ src/resources/index.ts
@@ -0,0 +1,24 @@
+export const loadResource = async () => {
+  const fontURLMap = import.meta.glob<boolean, string, string>(
+    "./fonts/*.ttf",
+    {
+      query: "?url",
+      import: "default",
+    },
+  );
+
+  await Promise.all(
+    Object.values(fontURLMap).map(async (getURL) => {
+      const url = await getURL();
+      const family = url.split("/").at(-1);
+      if (!family) return;
+      const fontface = new FontFace(
+        family.replace(/(-[A-Za-z0-9]+)?\..+/, ""),
+        `url(${url})`,
+      );
+      await fontface.load();
+      document.fonts.add(fontface);
+      return fontface;
+    }),
+  );
+};
